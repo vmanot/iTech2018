bccc70450e968c0c166937c1716920d9558ee699
Vatsal Manot <vatsal.manot@yahoo.com>
2018-12-08 11:17:15 +0530

    Initial Commit

---
+++ iTech2018/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iTech2018
+//
+//  Created by Vatsal Manot on 12/8/18.
+//  Copyright © 2018 Vatsal Manot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
